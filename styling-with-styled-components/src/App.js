3af15487217314a0ec320a38712880cf963dcb5a
song <johyesong8686@gmail.com>
2021-02-15 17:26:43 +0900

    Sass통한 버튼 사용

--- styling-with-styled-components/src/App.js
+++ styling-with-styled-components/src/App.js
@@ -1,20 +1,19 @@
 import logo from "./logo.svg";
 import styled, { css } from "styled-components";
-
-const Circle = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background: ${(props) => props.color || "red"};
-  border-radius: 50%;
-  ${(props) =>
-    props.huge &&
-    css`
-      width: 10rem;
-      height: 10rem;
-    `}
+import Button from "./components/Button";
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
 `;
 function App() {
-  return <Circle color="blue" huge />;
+  return (
+    <AppBlock>
+      <Button>BUTTON</Button>
+    </AppBlock>
+  );
 }
 
 export default App;
---
+++ styling-with-styled-components/src/components/Button.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+const StyledButton = styled.button`
+  /* 공통 스타일 */
+  display: inline-flex;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  /* 크기 */
+  height: 2.25rem;
+  font-size: 1rem;
+
+  background: #228be6;
+  &:hover {
+    background: #339af0;
+  }
+  &:active {
+    background: #1c7ed6;
+  }
+  & + & {
+    margin-left: 1rem;
+  }
+`;
+
+const Button = ({ children, ...rest }) => {
+  return <StyledButton>{children}</StyledButton>;
+};
+export default Button;
